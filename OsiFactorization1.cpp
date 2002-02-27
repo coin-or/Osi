5b3c3215ea892155ffcec17ce74c958196f664bb
anonymous <anonymous@coin-or.clemson.edu>
2002-02-27 16:32:58 +0000

    take out OsiFactorization2.ipp means a large OsiFactorization.hpp

--- OsiFactorization1.cpp
+++ OsiFactorization1.cpp
@@ -134,7 +134,7 @@ void OsiFactorization::gutsOfInitialize(int type)
 }
 //Part of LP
 int OsiFactorization::factorize (
-				 OsiPackedMatrix & matrix,
+				 const OsiPackedMatrix & matrix,
 				 int rowIsBasic[],
 				 int columnIsBasic[],
 				 double areaFactor )
@@ -634,304 +634,6 @@ OsiFactorization::factor (  )
   return status_;
 }
 
-//  factorSparse.  Does sparse phase of factorization
-//return code is <0 error, 0= finished
-int
-OsiFactorization::factorSparse (  )
-{
-  int status = 0;
-  int *indexRow = indexRowU_;
-  int *indexColumn = indexColumnU_;
-  double *element = elementU_;
-  int count = 1;
-  double *workArea = new double [ numberRows_ ];
-
-  CoinFillN ( workArea, numberRows_ , 0.0);
-  //get space for bit work area
-  OsiBigIndex workSize = 1000;
-  unsigned int *workArea2 = ( unsigned int * ) new int [ workSize ];
-  int lastColumnInBlock;
-
-  lastColumnInBlock = numberColumns_;
-  int larger;
-
-  if ( numberRows_ < numberColumns_ ) {
-    larger = numberColumns_;
-  } else {
-    larger = numberRows_;
-  }				
-  //do slacks first
-  int pivotColumn;
-  for ( pivotColumn = 0; pivotColumn < lastColumnInBlock;
-	pivotColumn++ ) {
-    if ( numberInColumn_[pivotColumn] == 1 ) {
-      OsiBigIndex start = startColumnU_[pivotColumn];
-      double value = element[start];
-
-      if ( value == slackValue_ && numberInColumnPlus_[pivotColumn] == 0 ) {
-	//treat as slack
-	int iRow = indexRowU_[start];
-
-	totalElements_--;
-	if ( !pivotColumnSingleton ( iRow, pivotColumn ) ) {
-	  return -99;
-	}
-	pivotColumn_[numberGoodU_] = pivotColumn;
-	numberGoodU_++;
-      }				
-    }				
-  }				
-  numberSlacks_ = numberGoodU_;
-  while ( count <= biggerDimension_ ) {
-    OsiBigIndex minimumCount = INT_MAX;
-    OsiBigIndex minimumCost = INT_MAX;
-
-    count = 1;
-    bool stopping = false;
-    int pivotRow = -1;
-    int pivotColumn = -1;
-    int pivotRowPosition = -1;
-    int pivotColumnPosition = -1;
-    int look = firstCount_[count];
-    int trials = 0;
-
-    while ( !stopping ) {
-      if ( count == 1 && firstCount_[1] >= 0 ) {
-	//do column singletons first to put more in U
-	while ( look >= 0 ) {
-	  if ( look >= numberRows_ ) {
-	    int iColumn = look - numberRows_;
-
-#if DEBUG_OSI
-	    if ( numberInColumn_[iColumn] != count ) {
-	      abort (  );
-	    }			
-#endif
-	    OsiBigIndex start = startColumnU_[iColumn];
-	    int iRow = indexRow[start];
-
-	    pivotRow = iRow;
-	    pivotRowPosition = start;
-	    pivotColumn = iColumn;
-	    pivotColumnPosition = -1;
-	    stopping = true;
-	    look = -1;
-	    break;
-	  } else {
-	    look = nextCount_[look];
-	  }			
-	}			/* endwhile */
-	if ( !stopping ) {
-	  //back to singletons
-	  look = firstCount_[1];
-	}			
-      }				
-      int *nextCount = nextCount_;
-      int *numberInRow = numberInRow_;
-      int *numberInColumn = numberInColumn_;
-      OsiBigIndex *startRow = startRowU_;
-      OsiBigIndex *startColumn = startColumnU_;
-      double pivotTolerance = pivotTolerance_;
-      int numberTrials = numberTrials_;
-      int numberRows = numberRows_;
-
-      while ( look >= 0 ) {
-	if ( look < numberRows_ ) {
-	  int iRow = look;
-
-#if DEBUG_OSI
-	  if ( numberInRow[iRow] != count ) {
-	    abort (  );
-	  }			
-#endif
-	  look = nextCount[look];
-	  bool rejected = false;
-	  OsiBigIndex start = startRow[iRow];
-	  OsiBigIndex end = start + count;
-
-	  OsiBigIndex i;
-	  for ( i = start; i < end; i++ ) {
-	    int iColumn = indexColumn[i];
-	    OsiBigIndex cost = ( count - 1 ) * numberInColumn[iColumn];
-
-	    if ( cost < minimumCost ) {
-	      OsiBigIndex where = startColumn[iColumn];
-	      double minimumValue = element[where];
-
-	      minimumValue = fabs ( minimumValue ) * pivotTolerance;
-	      while ( indexRow[where] != iRow ) {
-		where++;
-	      }			/* endwhile */
-#if DEBUG_OSI
-	      {
-		OsiBigIndex end_debug = startColumn[iColumn] +
-		  numberInColumn[iColumn];
-
-		if ( where >= end_debug ) {
-		  abort (  );
-		}		
-	      }
-#endif
-	      double value = element[where];
-
-	      value = fabs ( value );
-	      if ( value >= minimumValue ) {
-		minimumCost = cost;
-		minimumCount = numberInColumn[iColumn];
-		pivotRow = iRow;
-		pivotRowPosition = -1;
-		pivotColumn = iColumn;
-		pivotColumnPosition = i;
-		if ( minimumCount < count ) {
-		  stopping = true;
-		  look = -1;
-		  break;
-		}		
-	      } else if ( pivotRow == -1 ) {
-		rejected = true;
-	      }			
-	    }			
-	  }			
-	  trials++;
-	  if ( trials >= numberTrials && pivotRow >= 0 ) {
-	    stopping = true;
-	    look = -1;
-	    break;
-	  }			
-	  if ( rejected ) {
-	    //take out for moment
-	    //eligible when row changes
-	    deleteLink ( iRow );
-	    addLink ( iRow, biggerDimension_ + 1 );
-	  }			
-	} else {
-	  int iColumn = look - numberRows;
-
-#if DEBUG_OSI
-	  if ( numberInColumn[iColumn] != count ) {
-	    abort (  );
-	  }			
-#endif
-	  look = nextCount[look];
-	  OsiBigIndex start = startColumn[iColumn];
-	  OsiBigIndex end = start + numberInColumn[iColumn];
-	  double minimumValue = element[start];
-
-	  minimumValue = fabs ( minimumValue ) * pivotTolerance;
-	  OsiBigIndex i;
-	  for ( i = start; i < end; i++ ) {
-	    double value = element[i];
-
-	    value = fabs ( value );
-	    if ( value >= minimumValue ) {
-	      int iRow = indexRow[i];
-	      OsiBigIndex cost = ( count - 1 ) * numberInRow[iRow];
-
-	      if ( cost < minimumCost ) {
-		minimumCost = cost;
-		minimumCount = numberInRow[iRow];
-		pivotRow = iRow;
-		pivotRowPosition = i;
-		pivotColumn = iColumn;
-		pivotColumnPosition = -1;
-		if ( minimumCount <= count + 1 ) {
-		  stopping = true;
-		  look = -1;
-		  break;
-		}		
-	      }			
-	    }			
-	  }			
-	  trials++;
-	  if ( trials >= numberTrials && pivotRow >= 0 ) {
-	    stopping = true;
-	    look = -1;
-	    break;
-	  }			
-	}			
-      }				/* endwhile */
-      //end of this - onto next
-      if ( !stopping ) {
-	count++;
-	if ( count <= biggerDimension_ ) {
-	  look = firstCount_[count];
-	} else {
-	  stopping = true;
-	}			
-      } else {
-	if ( pivotRow >= 0 ) {
-	  int numberDoRow = numberInRow_[pivotRow] - 1;
-	  int numberDoColumn = numberInColumn_[pivotColumn] - 1;
-
-	  totalElements_ -= ( numberDoRow + numberDoColumn + 1 );
-	  if ( numberDoColumn > 0 ) {
-	    if ( numberDoRow > 0 ) {
-	      if ( numberDoColumn > 1 ) {
-		//  if (1) {
-		//need to adjust more for cache and SMP
-		//allow at least 4 extra
-		int increment = numberDoColumn + 1 + 4;
-
-		if ( increment & 15 ) {
-		  increment = increment & ( ~15 );
-		  increment += 16;
-		}		
-		int increment2 =
-
-		  ( increment + BITS_PER_INT - 1 ) >> SHIFT_PER_INT;
-		OsiBigIndex size = increment2 * numberDoRow;
-
-		if ( size > workSize ) {
-		  workSize = size;
-		  delete []  workArea2 ;
-		  workArea2 = ( unsigned int * ) new int [ workSize ];
-		}		
-		bool goodPivot;
-
-		if ( larger < 32766 ) {
-		  //branch out to best pivot routine 
-		  goodPivot = pivot ( pivotRow, pivotColumn,
-				      pivotRowPosition, pivotColumnPosition,
-				      workArea, workArea2, increment,
-				      increment2, ( short * ) markRow_ );
-		} else {
-		  //might be able to do better by permuting
-		  goodPivot = pivot ( pivotRow, pivotColumn,
-				      pivotRowPosition, pivotColumnPosition,
-				      workArea, workArea2, increment,
-				      increment2, ( int * ) markRow_ );
-		}		
-		if ( !goodPivot ) {
-		  return -99;
-		}
-	      } else {
-		if ( !pivotOneOtherRow ( pivotRow, pivotColumn ) ) {
-		  return -99;
-		}
-	      }			
-	    } else {
-	      if ( !pivotRowSingleton ( pivotRow, pivotColumn ) ) {
-		return -99;
-	      }
-	    }			
-	  } else {
-	    if ( !pivotColumnSingleton ( pivotRow, pivotColumn ) ) {
-	      return -99;
-	    }
-	  }			
-	  pivotColumn_[numberGoodU_] = pivotColumn;
-	  numberGoodU_++;
-	}			
-      }				
-    }				/* endwhile */
-#if DEBUG_OSI==2
-    checkConsistency (  );
-#endif
-  }				/* endwhile */
-  delete []  workArea ;
-  delete [] workArea2 ;
-  return status;
-}
 
 //  pivotRowSingleton.  Does one pivot on Row Singleton in factorization
 bool
@@ -1684,4 +1386,308 @@ OsiFactorization::checkConsistency (  )
   if ( bad ) {
     abort (  );
   }				
+}
+  //  pivotOneOtherRow.  When just one other row so faster
+bool 
+OsiFactorization::pivotOneOtherRow ( int pivotRow,
+					   int pivotColumn )
+{
+  int numberInPivotRow = numberInRow_[pivotRow] - 1;
+  OsiBigIndex startColumn = startColumnU_[pivotColumn];
+  OsiBigIndex startRow = startRowU_[pivotRow];
+  OsiBigIndex endRow = startRow + numberInPivotRow + 1;
+
+  //take out this bit of indexColumnU
+  int next = nextRow_[pivotRow];
+  int last = lastRow_[pivotRow];
+
+  nextRow_[last] = next;
+  lastRow_[next] = last;
+  nextRow_[pivotRow] = numberGoodU_;	//use for permute
+  lastRow_[pivotRow] = -2;
+  numberInRow_[pivotRow] = 0;
+  //store column in L, compress in U and take column out
+  OsiBigIndex l = lengthL_;
+
+  if ( l + 1 > lengthAreaL_ ) {
+    //need another area
+    cout << "code pivot 1" << endl;
+    //leave gap so starts will work
+    abort (  );
+  }				
+  //l+=currentAreaL_->elementByColumn-elementL_;
+  //OsiBigIndex lSave=l;
+  pivotRowL_[numberGoodL_] = pivotRow;
+  startColumnL_[numberGoodL_] = l;	//for luck and first time
+  numberGoodL_++;
+  startColumnL_[numberGoodL_] = l + 1;
+  lengthL_++;
+  double pivotElement;
+  double otherMultiplier;
+  int otherRow;
+
+  if ( indexRowU_[startColumn] == pivotRow ) {
+    pivotElement = elementU_[startColumn];
+    otherMultiplier = elementU_[startColumn + 1];
+    otherRow = indexRowU_[startColumn + 1];
+  } else {
+    pivotElement = elementU_[startColumn + 1];
+    otherMultiplier = elementU_[startColumn];
+    otherRow = indexRowU_[startColumn];
+  }				
+  int numberSave = numberInRow_[otherRow];
+  double pivotMultiplier = 1.0 / pivotElement;
+
+  pivotRegion_[numberGoodU_] = pivotMultiplier;
+  numberInColumn_[pivotColumn] = 0;
+  otherMultiplier = otherMultiplier * pivotMultiplier;
+  indexRowL_[l] = otherRow;
+  elementL_[l] = otherMultiplier;
+  //take out of row list
+  OsiBigIndex start = startRowU_[otherRow];
+  OsiBigIndex end = start + numberSave;
+  OsiBigIndex where = start;
+
+  while ( indexColumnU_[where] != pivotColumn ) {
+    where++;
+  }				/* endwhile */
+#if DEBUG_OSI
+  if ( where >= end ) {
+    abort (  );
+  }				
+#endif
+  end--;
+  indexColumnU_[where] = indexColumnU_[end];
+  int numberAdded = 0;
+  int numberDeleted = 0;
+
+  //pack down and move to work
+  int j;
+
+  for ( j = startRow; j < endRow; j++ ) {
+    int iColumn = indexColumnU_[j];
+
+    if ( iColumn != pivotColumn ) {
+      OsiBigIndex startColumn = startColumnU_[iColumn];
+      OsiBigIndex endColumn = startColumn + numberInColumn_[iColumn];
+      int iRow = indexRowU_[startColumn];
+      double value = elementU_[startColumn];
+      double largest;
+      bool foundOther = false;
+
+      //leave room for pivot
+      OsiBigIndex put = startColumn + 1;
+      OsiBigIndex positionLargest = -1;
+      double thisPivotValue = 0.0;
+      double otherElement = 0.0;
+      double nextValue = elementU_[put];;
+      int nextIRow = indexRowU_[put];
+
+      //compress column and find largest not updated
+      if ( iRow != pivotRow ) {
+	if ( iRow != otherRow ) {
+	  largest = fabs ( value );
+	  elementU_[put] = value;
+	  indexRowU_[put] = iRow;
+	  positionLargest = put;
+	  put++;
+	  OsiBigIndex i;
+	  for ( i = startColumn + 1; i < endColumn; i++ ) {
+	    iRow = nextIRow;
+	    value = nextValue;
+#ifdef CHECKING
+	    // doesn't matter reading uninitialized but annoys checking
+	    if ( i + 1 < endColumn ) {
+#endif
+	      nextIRow = indexRowU_[i + 1];
+	      nextValue = elementU_[i + 1];
+#ifdef CHECKING
+	    }
+#endif
+	    if ( iRow != pivotRow ) {
+	      if ( iRow != otherRow ) {
+		//keep
+		indexRowU_[put] = iRow;
+		elementU_[put] = value;;
+		put++;
+	      } else {
+		otherElement = value;
+		foundOther = true;
+	      }			
+	    } else {
+	      thisPivotValue = value;
+	    }			
+	  }			
+	} else {
+	  otherElement = value;
+	  foundOther = true;
+	  //need to find largest
+	  largest = 0.0;
+	  OsiBigIndex i;
+	  for ( i = startColumn + 1; i < endColumn; i++ ) {
+	    iRow = nextIRow;
+	    value = nextValue;
+#ifdef CHECKING
+	    // doesn't matter reading uninitialized but annoys checking
+	    if ( i + 1 < endColumn ) {
+#endif
+	      nextIRow = indexRowU_[i + 1];
+	      nextValue = elementU_[i + 1];
+#ifdef CHECKING
+	    }
+#endif
+	    if ( iRow != pivotRow ) {
+	      //keep
+	      indexRowU_[put] = iRow;
+	      elementU_[put] = value;;
+	      double absValue = fabs ( value );
+
+	      if ( absValue > largest ) {
+		largest = absValue;
+		positionLargest = put;
+	      }			
+	      put++;
+	    } else {
+	      thisPivotValue = value;
+	    }			
+	  }			
+	}			
+      } else {
+	//need to find largest
+	largest = 0.0;
+	thisPivotValue = value;
+	OsiBigIndex i;
+	for ( i = startColumn + 1; i < endColumn; i++ ) {
+	  iRow = nextIRow;
+	  value = nextValue;
+#ifdef CHECKING
+	  // doesn't matter reading uninitialized but annoys checking
+	  if ( i + 1 < endColumn ) {
+#endif
+	    nextIRow = indexRowU_[i + 1];
+	    nextValue = elementU_[i + 1];
+#ifdef CHECKING
+	  }
+#endif
+	  if ( iRow != otherRow ) {
+	    //keep
+	    indexRowU_[put] = iRow;
+	    elementU_[put] = value;;
+	    double absValue = fabs ( value );
+
+	    if ( absValue > largest ) {
+	      largest = absValue;
+	      positionLargest = put;
+	    }			
+	    put++;
+	  } else {
+	    otherElement = value;
+	    foundOther = true;
+	  }			
+	}			
+      }				
+      //slot in pivot
+      elementU_[startColumn] = thisPivotValue;
+      indexRowU_[startColumn] = pivotRow;
+      //clean up counts
+      startColumn++;
+      numberInColumn_[iColumn] = put - startColumn;
+      numberInColumnPlus_[iColumn]++;
+      startColumnU_[iColumn]++;
+      otherElement = otherElement - thisPivotValue * otherMultiplier;
+      double absValue = fabs ( otherElement );
+
+      if ( absValue > zeroTolerance_ ) {
+	if ( !foundOther ) {
+	  //have we space
+	  saveColumn_[numberAdded++] = iColumn;
+	  int next = nextColumn_[iColumn];
+	  OsiBigIndex space;
+
+	  space = startColumnU_[next] - put - numberInColumnPlus_[next];
+	  if ( space <= 0 ) {
+	    //getColumnSpace also moves fixed part
+	    int number = numberInColumn_[iColumn];
+
+	    if ( !getColumnSpace ( iColumn, number + 1 ) ) {
+	      return false;
+	    }
+	    //redo starts
+	    positionLargest =
+	      positionLargest + startColumnU_[iColumn] - startColumn;
+	    startColumn = startColumnU_[iColumn];
+	    put = startColumn + number;
+	  }			
+	}			
+	elementU_[put] = otherElement;
+	indexRowU_[put] = otherRow;
+	if ( absValue > largest ) {
+	  largest = absValue;
+	  positionLargest = put;
+	}			
+	put++;
+      } else {
+	if ( foundOther ) {
+	  numberDeleted++;
+	  //take out of row list
+	  OsiBigIndex where = start;
+
+	  while ( indexColumnU_[where] != iColumn ) {
+	    where++;
+	  }			/* endwhile */
+#if DEBUG_OSI
+	  if ( where >= end ) {
+	    abort (  );
+	  }			
+#endif
+	  end--;
+	  indexColumnU_[where] = indexColumnU_[end];
+	}			
+      }				
+      numberInColumn_[iColumn] = put - startColumn;
+      //move largest
+      if ( positionLargest >= 0 ) {
+	value = elementU_[positionLargest];
+	iRow = indexRowU_[positionLargest];
+	elementU_[positionLargest] = elementU_[startColumn];
+	indexRowU_[positionLargest] = indexRowU_[startColumn];
+	elementU_[startColumn] = value;
+	indexRowU_[startColumn] = iRow;
+      }				
+      //linked list for column
+      if ( nextCount_[iColumn + numberRows_] != -2 ) {
+	//modify linked list
+	deleteLink ( iColumn + numberRows_ );
+	addLink ( iColumn + numberRows_, numberInColumn_[iColumn] );
+      }				
+    }				
+  }				
+  //get space for row list
+  next = nextRow_[otherRow];
+  OsiBigIndex space;
+
+  space = startRowU_[next] - end;
+  totalElements_ += numberAdded - numberDeleted;
+  int number = numberAdded + ( end - start );
+
+  if ( space < numberAdded ) {
+    numberInRow_[otherRow] = end - start;
+    if ( !getRowSpace ( otherRow, number ) ) {
+      return false;
+    }
+    end = startRowU_[otherRow] + end - start;
+  }				
+  // do linked lists and update counts
+  numberInRow_[otherRow] = number;
+  if ( number != numberSave ) {
+    deleteLink ( otherRow );
+    addLink ( otherRow, number );
+  }				
+  for ( j = 0; j < numberAdded; j++ ) {
+    indexColumnU_[end++] = saveColumn_[j];
+  }				
+  //modify linked list for pivots
+  deleteLink ( pivotRow );
+  deleteLink ( pivotColumn + numberRows_ );
+  return true;
 }

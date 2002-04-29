ee8a32ee8a3370e845327641fb4d923331bfd347
JP Fasano <jpfasano@gmail.com>
2002-04-29 17:23:11 +0000

    Modifed with Lou Hafer's changes to consider NAN and finite when making comparisons. These changes are needed for Dylp.

--- include/OsiFloatEqual.hpp
+++ include/OsiFloatEqual.hpp
@@ -3,137 +3,168 @@
 #ifndef OsiFloatEqual_H
 #define OsiFloatEqual_H
 
-//#include <stdlib.h>
-//#include <stdio.h>
 #include <algorithm>
-//#include <utilities>
-//#include <limits>
-//#include <numeric>
 #include <cmath>
 
+#if defined(_MSC_VER)
+# include<float.h>
+# define isnan   _isnan
+# define finite  _finite
+#endif
+
+#ifdef sun
+  extern "C" int finite(double),isnan(double) ;
+#endif
 
 
-//------------------------------------------------------
-//
-// Function objects for testing equality of floats
-//
-//------------------------------------------------------
+/*! \file OsiFloatEqual.hpp
+    \brief Function objects for testing equality of real numbers.
 
-/** Function object for testing doubles for equality.
+  Two objects are provided; one tests for equality to an absolute tolerance,
+  one to a scaled tolerance. The tests will handle IEEE floating point, but
+  note that infinity == infinity. Mathematicians are rolling in their graves,
+  but this matches the behaviour for the common practice of using DBL_MAX
+  (numeric_limits<double>::max(), or similar large finite number) as infinity.
 
-Operands are considered equal if their difference is 
-within an epsilon.
-This test does not consider the relative magnitude of 
-the operands.<br>
-Example usage:
-<pre>
-   double d1=3.14159;
-   double d2=d1;
-   double d3=d1+.0001;
-   OsiAbsFltEq eq1;
-   OsiAbsFltEq eq2(.001);
-   assert(  eq1(d1,d2) );
-   assert( !eq1(d1,d3) );
-   assert(  eq2(d1,d3) );
-</pre>
+  <p>
+  Example usage:
+  <pre>
+    double d1 = 3.14159 ;
+    double d2 = d1 ;
+    double d3 = d1+.0001 ;
+
+    OsiAbsFltEq eq1 ;
+    OsiAbsFltEq eq2(.001) ;
+
+    assert(  eq1(d1,d2) ) ;
+    assert( !eq1(d1,d3) ) ;
+    assert(  eq2(d1,d3) ) ;
+  </pre>
+  OsiRelFltEq follows the same pattern.
 */
+
+/*! \brief Equality to an absolute tolerance
+
+  Operands are considered equal if their difference is within an epsilon ;
+  the test does not consider the relative magnitude of the operands.
+*/
+
 class OsiAbsFltEq
 {
-public:
-  /// Compare function
-  inline bool operator()(double f1, double f2) const
-  { return fabs(f1-f2) < epsilon_; };
+  public:
 
-  /**@name Constructors and destructors */
+  //! Compare function
+
+  inline bool operator() (const double f1, const double f2) const
+
+  { if (isnan(f1) || isnan(f2)) return false ;
+    if (f1 == f2) return true ;
+    return (fabs(f1-f2) < epsilon_) ; } ;
+
+  /*! \name Constructors and destructors */
   //@{
-  /// Default constructor
-  OsiAbsFltEq():epsilon_(1.e-10){}; 
 
-  /// Alternate constructor. Allows setting epsilon
-  OsiAbsFltEq(double epsilon):epsilon_(epsilon){}; 
+  //! Default constructor
 
-  /// Destructor
-  virtual ~OsiAbsFltEq(){};   
+  OsiAbsFltEq () : epsilon_(1.e-10) {} ;
 
-  /// Copy constructor
-  OsiAbsFltEq(const OsiAbsFltEq & src):epsilon_(src.epsilon_){}; 
+  //! Alternate constructor with epsilon as a parameter
 
-  /// Assignment
-  OsiAbsFltEq & operator=(const OsiAbsFltEq& rhs)
-  {
-    if (this != &rhs) {	
-      epsilon_ = rhs.epsilon_;
-    }
-    return *this;
-  };
+  OsiAbsFltEq (const double epsilon) : epsilon_(epsilon) {} ;
+
+  //! Destructor
+
+  virtual ~OsiAbsFltEq () {} ;
+
+  //! Copy constructor
+
+  OsiAbsFltEq (const OsiAbsFltEq& src) : epsilon_(src.epsilon_) {} ;
+
+  //! Assignment
+
+  OsiAbsFltEq& operator= (const OsiAbsFltEq& rhs)
+
+  { if (this != &rhs) epsilon_ = rhs.epsilon_ ;
+    return (*this) ; } ;
+
   //@}
-private:  
-  /**@name Private member data */
+
+  private:  
+
+  /*! \name Private member data */
   //@{
-  /// Maximum allowed difference between operands for them to be considered equal
-  double epsilon_;
+
+  //! Equality tolerance.
+
+  double epsilon_ ;
+
   //@}
-};
+
+} ;
 
 
-/** Function object for testing doubles for eqaulity.
 
-This test considers the relative magnitude of the operands.
-Two operands f1 & f2 are considered equal if<br>
-abs(f1-f2) &lt;= epsilon*(max(abs(f1),abs(f2))+1)<br>
-Example usage:<br>
-<pre>
-   double d1=3.14159;
-   double d2=d1;
-   double d3=d1+.0001;
-   OsiRelFltEq eq1;
-   assert(  eq1(d1,d2) );
-   assert( !eq1(d1,d3) );
-</pre>
+/*! \brief Equality to a scaled tolerance
+
+  Operands are considered equal if their difference is within a scaled
+  epsilon calculated as epsilon_*(1+max(|f1|,|f2|)).
 */
+
 class OsiRelFltEq
 {
-public:
-  /// Compare function
-  inline bool operator()(double f1, double f2) const
-  { 
-    //double tol = std::max(std::abs(f1),std::abs(f2)) + 1.;
-    //return std::abs(f1-f2) <= epsilon_*tol;
-    double tol = fabs(f1) > fabs(f2) ? fabs(f1) : fabs(f2);
-    tol++;
-    return fabs(f1-f2) <= epsilon_*tol; 
-  };
+  public:
 
-  /**@name Constructors and destructors */
+  //! Compare function
+
+  inline bool operator() (const double f1, const double f2) const
+
+  { if (isnan(f1) || isnan(f2)) return false ;
+    if (f1 == f2) return true ;
+    if (!finite(f1) || !finite(f2)) return false ;
+
+    double tol = (fabs(f1)>fabs(f2))?fabs(f1):fabs(f2) ;
+
+    return (fabs(f1-f2) <= epsilon_*(1+tol)) ; }
+
+  /*! \name Constructors and destructors */
   //@{
-  /// Default constructor
-  OsiRelFltEq():epsilon_(1.e-10){}; 
 
-  /// Alternate constructor. Allows setting epsilon.
-  OsiRelFltEq(double epsilon):epsilon_(epsilon){}; 
+  //! Default constructor
 
-  /// Destructor
-  virtual ~OsiRelFltEq(){};   
+  OsiRelFltEq () : epsilon_(1.e-10) {} ;
 
-  /// Copy constructor
-  OsiRelFltEq(const OsiRelFltEq & src):epsilon_(src.epsilon_){}; 
+  //! Alternate constructor with epsilon as a parameter
 
-  /// Assignment
-  OsiRelFltEq & operator=(const OsiRelFltEq& rhs)
-  {
-    if (this != &rhs) {	
-      epsilon_ = rhs.epsilon_;
-    }
-    return *this;
-  };
+  OsiRelFltEq (const double epsilon) : epsilon_(epsilon) {} ;
+
+  //! Destructor
+
+  virtual ~OsiRelFltEq () {} ;
+
+  //! Copy constructor
+
+  OsiRelFltEq (const OsiRelFltEq & src) : epsilon_(src.epsilon_) {} ;
+
+  //! Assignment
+
+  OsiRelFltEq& operator= (const OsiRelFltEq& rhs)
+
+  { if (this != &rhs) epsilon_ = rhs.epsilon_ ;
+    return (*this) ; } ;
+
   //@}
 
 private: 
-  /**@name Private member data */
+
+  /*! \name Private member data */
   //@{
-  /// Epsilon
-  double epsilon_;
+
+  //! Base equality tolerance
+
+  double epsilon_ ;
+
   //@}
-};
+
+} ;
 
 #endif

b843f0826965b0830e059f3b92838162b0e40d15
Ted Ralphs <ted@lehigh.edu>
2006-10-10 03:21:00 +0000

    Fixing minor compilation problem

--- Osi/src/OsiSym/OsiSymSolverInterface.hpp
+++ Osi/src/OsiSym/OsiSymSolverInterface.hpp
@@ -21,10 +21,6 @@
 
 #include "OsiSolverInterface.hpp"
 #include "OsiSymSolverParameters.hpp"
-/* Switch off part of PROTO as seems to cause problems on some architectures.
-   and not needed in interface ? */
-#define HAS_SRANDOM
-#define HAS_RANDOM
 #include "SymWarmStart.hpp"
 #include "symphony_api.h"
 

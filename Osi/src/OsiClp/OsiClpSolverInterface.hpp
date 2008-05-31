5a7787f4c144db72932507ca5260eacf51124c38
John Forrest <john.forrest@fastercoin.com>
2008-05-31 09:43:05 +0000

    add lastAlgorithm

--- Osi/src/OsiClp/OsiClpSolverInterface.hpp
+++ Osi/src/OsiClp/OsiClpSolverInterface.hpp
@@ -925,6 +925,9 @@ public:
   inline unsigned int specialOptions() const
   { return specialOptions_;}
   void setSpecialOptions(unsigned int value);
+  /// Last algorithm used , 1 = primal, 2 = dual
+  inline int lastAlgorithm() const
+  { return lastAlgorithm_;}
   /// Get scaling action option
   inline int cleanupScaling() const
   { return cleanupScaling_;}
@@ -1146,7 +1149,7 @@ protected:
   /** The original iteration limit before hotstarts started. */
   int itlimOrig_;
   
-  /// Last algorithm used
+  /// Last algorithm used , 1 = primal, 2 = dual
   mutable int lastAlgorithm_;
   
   /// To say if destructor should delete underlying model
@@ -1271,6 +1274,7 @@ public:
   /// are we in trouble
   inline bool inTrouble() const
   { return inTrouble_;}
+  
   //@}
   
   
